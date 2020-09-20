e11a43e115cb76c301ce44d1702a7ae4e4c2a698
luandenale <luandenale@hotmail.com>
2020-09-20 11:15:31 -0300

    -> Draft of implementation;

---
+++ Assets/Scripts/System Utilities/Variables Management/ScriptableObjects/AudioLibraryScriptableObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VariableManagement
+{
+    public enum AudioName
+    {
+        BASHER_FOOTSTEPS,
+        DOOR_OPEN,
+        AUDIO_NOVO
+    }
+
+    [System.Serializable]
+    public class AudioClipUnit
+    {
+        public AudioName audioName;
+        public AudioClip audioFile;
+        public float volume;
+    }
+
+    [CreateAssetMenu(fileName = "AudioLibrary")]
+    public class AudioLibraryScriptableObject : ScriptableObject
+    {
+        public string steamID;
+        public string xboxID;
+        public string ps4ID;        
+    }
+}
