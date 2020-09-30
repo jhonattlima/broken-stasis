6e319026b922587a31aa9927160dadb5bdeeb61b
jhonattlima <jhonattlima@gmail.com>
2020-09-29 21:35:32 -0300

    Solved index problem

--- Assets/Scripts/System Utilities/Audio/AudioManager.cs
+++ Assets/Scripts/System Utilities/Audio/AudioManager.cs
@@ -2,23 +2,22 @@
 
 namespace Audio
 {
-    public class AudioManager: MonoBehaviour
+    public class AudioManager : MonoBehaviour
     {
         private static AudioLibraryScriptableObject _audioLibrary;
 
         private static AudioManager _instance;
         private static GameObject _audioManagerGameObject;
         private static AudioSourcePool _audioSourcePool;
-
+        
         public static AudioManager instance
         {
             get
             {
-                if(_instance == null)
+                if (_instance == null)
                 {
                     InitializeAudioManager();
                 }
-
                 return _instance;
             }
         }
@@ -26,12 +25,13 @@ namespace Audio
         private static void InitializeAudioManager()
         {
             _audioLibrary = Resources.Load<AudioLibraryScriptableObject>("AudioLibrary");
-            
-            _audioManagerGameObject = Instantiate(new GameObject("AudioManager"));
+
+            _audioManagerGameObject = new GameObject("AudioManager");
             _audioManagerGameObject.AddComponent<AudioManager>();
 
-            _audioSourcePool = new AudioSourcePool(
-                Instantiate(new GameObject("AudioSourcePool"), _audioManagerGameObject.gameObject.transform).transform);
+            if (_audioSourcePool == null)
+                // _audioSourcePool = new AudioSourcePool(
+                //     Instantiate(new GameObject("AudioSourcePool"), _audioManagerGameObject.gameObject.transform).transform);
 
             _instance = _audioManagerGameObject.GetComponent<AudioManager>();
             DontDestroyOnLoad(_audioManagerGameObject);
@@ -40,8 +40,15 @@ namespace Audio
         public void Play(AudioNameEnum p_audio, bool p_loop)
         {
             AudioSource __audioSource = _audioSourcePool.GetFreeAudioSource();
-            AudioClipParams __audioClipParams = _audioLibrary.AudioLibrary[_audioLibrary.AudioLibrary.FindIndex(__index => __index.audioName.Equals(p_audio.ToString()))].audioClipParams;
 
+            AudioClipParams __audioClipParams = _audioLibrary.AudioLibrary.Find(clip => clip.audioName.Equals(p_audio.ToString())).audioClipParams;
+            
+            if(!__audioClipParams) 
+            {
+                Debug.LogError("Audio manager: audioclip not found: " + p_audio.ToString());
+                return;
+            }
+            
             __audioSource.loop = p_loop;
             __audioSource.clip = __audioClipParams.audioFile;
             __audioSource.volume = __audioClipParams.volume;
--- Assets/Scripts/System Utilities/Audio/AudioSourcePool.cs
+++ Assets/Scripts/System Utilities/Audio/AudioSourcePool.cs
@@ -7,8 +7,9 @@ namespace Audio
     {
         private List<AudioSource> _sourcesPool = new List<AudioSource>();
         private Transform _poolParent;
+        private GameObject _audiosourcePool;
 
-        public AudioSourcePool(Transform p_poolParent)
+        public AudioSourcePool(GameObject p_audioSourcePool, Transform p_poolParent)
         {
             _poolParent = p_poolParent;
 
@@ -18,18 +19,19 @@ namespace Audio
         private AudioSource InstantiateNewAudioSource()
         {
             GameObject __newAudioSourceGameObject = MonoBehaviour.Instantiate(new GameObject("AudioSource " + _sourcesPool.Count + 1), _poolParent);
+
             __newAudioSourceGameObject.AddComponent<AudioSource>();
-            
+
             _sourcesPool.Add(__newAudioSourceGameObject.GetComponent<AudioSource>());
 
             return _sourcesPool[_sourcesPool.Count - 1];
         }
 
         public AudioSource GetFreeAudioSource()
-        {          
-            foreach(AudioSource __audioSource in _sourcesPool)
+        {
+            foreach (AudioSource __audioSource in _sourcesPool)
             {
-                if(!__audioSource.isPlaying)
+                if (!__audioSource.isPlaying)
                     return __audioSource;
             }
 
