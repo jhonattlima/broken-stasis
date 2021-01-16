7554b3a4d2b004bd76c53271c386adf82c3152a7
jhonattlima <jhonattlima@gmail.com>
2021-01-16 14:35:32 -0300

    -> Story 371: Creating SuitChangeController

--- Assets/Scripts/GameManagers/GameplayManager.cs
+++ Assets/Scripts/GameManagers/GameplayManager.cs
@@ -10,7 +10,8 @@ namespace GameManagers
 {
     public class GameplayManager : MonoBehaviour
     {
-        [SerializeField] private PlayerContainer _playerContainer;
+        [SerializeField] private PlayerContainer _nakedPlayerContainer;
+        [SerializeField] private PlayerContainer _suit1PlayerContainer;
         [SerializeField] private CameraContainer _cameraContainer;
         [SerializeField] private GameObject _levelGameObjects;
         [SerializeField] private GameObject _enemiesGameObjects;
@@ -19,26 +20,37 @@ namespace GameManagers
         private CameraFollowPlayer _cameraFollowPlayer;
         private LevelObjectManager _levelObjectManager;
         private EnemiesManager _enemiesManager;
+        private PlayerSuitEnum _playerSuitEnum;
+        public static GameplayManager instance;
 
         private void Awake()
         {
-            LoadingView.instance.FadeOut(delegate()
+            if (instance == null)
+                instance = this;
+
+            LoadingView.instance.FadeOut(delegate ()
             {
                 InputController.GamePlay.InputEnabled = true;
             });
 
-            RegisterObjectsGraph();
+            RegisterObjectsGraph(_nakedPlayerContainer);
+            _playerSuitEnum = PlayerSuitEnum.NAKED;
 
             _player?.InitializePlayer();
             _enemiesManager?.InitializeEnemies(_player.onPlayerDamaged);
         }
 
-        private void RegisterObjectsGraph()
+        private void RegisterObjectsGraph(PlayerContainer p_playercontainer)
         {
-            if(_playerContainer != null) _player = new PlayerBase(_playerContainer);
-            if(_playerContainer != null && _cameraContainer != null) _cameraFollowPlayer = new CameraFollowPlayer(_playerContainer.playerTransform, _cameraContainer.cameraTransform);
-            if(_levelGameObjects != null) _levelObjectManager = new LevelObjectManager(_levelGameObjects.GetComponentsInChildren<IInteractionObject>().ToList());
-            if(_enemiesGameObjects != null) _enemiesManager = new EnemiesManager(_enemiesGameObjects.GetComponentsInChildren<IEnemy>().ToList());
+            RegisterPlayerGraph(p_playercontainer);
+            if (_levelGameObjects != null) _levelObjectManager = new LevelObjectManager(_levelGameObjects.GetComponentsInChildren<IInteractionObject>().ToList());
+            if (_enemiesGameObjects != null) _enemiesManager = new EnemiesManager(_enemiesGameObjects.GetComponentsInChildren<IEnemy>().ToList());
+        }
+
+        private void RegisterPlayerGraph(PlayerContainer p_playercontainer)
+        {
+            if (p_playercontainer != null) _player = new PlayerBase(p_playercontainer);
+            if (p_playercontainer != null && _cameraContainer != null) _cameraFollowPlayer = new CameraFollowPlayer(p_playercontainer.playerTransform, _cameraContainer.cameraTransform);
         }
 
         private void FixedUpdate()
@@ -52,6 +64,24 @@ namespace GameManagers
         {
             _levelObjectManager?.RunUpdate();
             _enemiesManager?.RunUpdate();
+        }
+
+        public void ChangePlayerGameObject(PlayerSuitEnum p_playersuitEnum)
+        {
+            if(p_playersuitEnum == _playerSuitEnum) return;
+
+            switch (p_playersuitEnum)
+            {
+                case PlayerSuitEnum.NAKED:
+                {
+                    break;
+                }
+                case PlayerSuitEnum.SUIT1:
+                {
+                    break;
+                }
+            }
+            _playerSuitEnum = p_playersuitEnum;
         }
     }
 }
---
+++ Assets/Scripts/Player/SuitChangeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuitChangeController
+{
+
+    private readonly GameObject nakedGameObject;
+    private readonly GameObject suit1GameObject;
+
+    // private readonly PlayerContainer nakedPlayerContainer;
+    // private readonly PlayerContainer suit1PlayerContainer;
+
+
+    public void ChangeSuit() {
+        // Camera faz fade out;
+        // toca som de roupa;
+        // muda o player Container no gameplay manager;
+        //
+        // desabilita modelo;
+        // habilita modelo com nova roupa;
+        // seta posicao do modelo anterior;
+        // camera faz fade in;
+    }
+
+}
