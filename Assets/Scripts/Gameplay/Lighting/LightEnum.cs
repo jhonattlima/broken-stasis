72703636755b26d860143be498d0e854f6affacd
luandenale <luandenale@hotmail.com>
2021-03-25 20:30:22 -0300

    -> Implemented lightcontroller;

--- Assets/Scripts/Gameplay/Lighting/LightEnum.cs
+++ Assets/Scripts/Gameplay/Lighting/LightEnum.cs
@@ -4,8 +4,9 @@
     {
         OFF,
         REGULAR_IDLE,
-        LOW_ILUMINATION_IDLE,
         REGULAR_FLICKING,
+        LOW_ILUMINATION_IDLE,
+        LOW_ILUMINATION_FLICKING,
         EMERGENCY_IDLE,
         EMERGENCY_LOOP
     }
